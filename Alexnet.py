e924e52e98b600b3a6f6c61e4421d799c2819b9b
lfalive <542679373@qq.com>
2020-11-15 16:33:45 +0800

    update AlexNet
    Signed-off-by: lfalive <542679373@qq.com>

--- Alexnet.py
+++ Alexnet.py
@@ -94,39 +94,67 @@ test_loader = DataLoader(
 )
 
 
-class Net(nn.Module):
+class AlexNet(nn.Module):
 	def __init__(self):
-		super(Net, self).__init__()
-		self.conv1 = nn.Conv2d(1, 20, 5, 1)
-		self.BN1 = nn.BatchNorm2d(20)
-		self.conv2 = nn.Conv2d(20, 50, 5, 1)
-		self.BN2 = nn.BatchNorm2d(50)
-		self.fc1 = nn.Linear(4 * 4 * 50, 500)
-		self.fc2 = nn.Linear(500, 10)
+		super(AlexNet, self).__init__()
+
+		# 由于MNIST为28x28， 而最初AlexNet的输入图片是227x227的。所以网络层数和参数需要调节
+		self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)  # AlexCONV1(3,96, k=11,s=4,p=0)
+		self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)  # AlexPool1(k=3, s=2)
+		self.relu1 = nn.ReLU()
+
+		# self.conv2 = nn.Conv2d(96, 256, kernel_size=5,stride=1,padding=2)
+		self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)  # AlexCONV2(96, 256,k=5,s=1,p=2)
+		self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)  # AlexPool2(k=3,s=2)
+		self.relu2 = nn.ReLU()
+
+		self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)  # AlexCONV3(256,384,k=3,s=1,p=1)
+		# self.conv4 = nn.Conv2d(384, 384, kernel_size=3, stride=1, padding=1)
+		self.conv4 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)  # AlexCONV4(384, 384, k=3,s=1,p=1)
+		self.conv5 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)  # AlexCONV5(384, 256, k=3, s=1,p=1)
+		self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)  # AlexPool3(k=3,s=2)
+		self.relu3 = nn.ReLU()
+
+		self.fc6 = nn.Linear(256 * 3 * 3, 1024)  # AlexFC6(256*6*6, 4096)
+		self.fc7 = nn.Linear(1024, 512)  # AlexFC6(4096,4096)
+		self.fc8 = nn.Linear(512, 10)  # AlexFC6(4096,1000)
 
 	def forward(self, x):
-		x = f.relu(self.BN1(self.conv1(x)))
-		x = f.max_pool2d(x, 2, 2)
-		x = f.relu(self.BN2(self.conv2(x)))
-		x = f.max_pool2d(x, 2, 2)
-		x = x.view(-1, 4 * 4 * 50)
-		x = f.relu(self.fc1(x))
-		x = self.fc2(x)
+		x = self.conv1(x)
+		x = self.pool1(x)
+		x = self.relu1(x)
+		x = self.conv2(x)
+		x = self.pool2(x)
+		x = self.relu2(x)
+		x = self.conv3(x)
+		x = self.conv4(x)
+		x = self.conv5(x)
+		x = self.pool3(x)
+		x = self.relu3(x)
+		x = x.view(-1, 256 * 3 * 3)  # Alex: x = x.view(-1, 256*6*6)
+		x = self.fc6(x)
+		x = f.relu(x)
+		x = self.fc7(x)
+		x = f.relu(x)
+		x = self.fc8(x)
 		return f.log_softmax(x, dim=1)
 
 
-model = Net().to(device)
+model = AlexNet().to(device)
 print(model)
 
 
 def get_next_batches(fdataloader: sy.FederatedDataLoader, nr_batches: int):
 	"""retrieve next nr_batches of the federated data loader and group the batches by worker
+
     Args:
         fdataloader (sy.FederatedDataLoader): federated data loader
         over which the function will iterate
         nr_batches (int): number of batches (per worker) to retrieve
+
     Returns:
         Dict[syft.workers.BaseWorker, List[batches]]
+
     """
 	batches = {}
 	for worker_id in fdataloader.workers:
@@ -144,6 +172,7 @@ def get_next_batches(fdataloader: sy.FederatedDataLoader, nr_batches: int):
 
 def train_on_batches(worker, batches, model_in, device, lr):
 	"""Train the model on the worker on the provided batches
+
     Args:
         worker(syft.workers.BaseWorker): worker on which the
         training will be executed
@@ -151,12 +180,14 @@ def train_on_batches(worker, batches, model_in, device, lr):
         model_in: machine learning model, training will be done on a copy
         device (torch.device): where to run the training
         lr: learning rate of the training steps
+
     Returns:
         model, loss: obtained model and loss after training
+
     """
 	model = model_in.copy()
-	# optimizer = optim.SGD(model.parameters(), lr=lr)
-	optimizer = optim.Adam(model.parameters(), lr=lr, betas=(0.9, 0.99))
+	optimizer = optim.SGD(model.parameters(), lr=lr)
+	# optimizer = optim.Adam(model.parameters(), lr=lr, betas=(0.9, 0.99))
 
 	model.train()
 	model.send(worker)
